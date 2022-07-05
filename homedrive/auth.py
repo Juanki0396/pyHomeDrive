3593bda9bb13628cd53257bf6d19332796567dd4
Juanki0396 <juanki0396@gmail.com>
2022-07-05 20:03:45 +0200

    Refactor code according to google style.

--- homedrive/auth.py
+++ homedrive/auth.py
@@ -1,17 +1,24 @@
 import functools
+import sqlite3
 
-from flask import current_app, Blueprint, render_template, request, session, url_for
+import flask
+from werkzeug import security
 
 from .db import get_db
 
-bp = Blueprint("auth", __name__, url_prefix="/auth")
+auth = flask.Blueprint("auth", __name__, url_prefix="/auth")
 
 
-@bp.route("/login")
-def login_page():
-    return render_template("login.html")
+@auth.route("/login")
+def login():
+    return flask.render_template("login.html")
 
 
-@bp.route("/register")
-def register_page():
-    return render_template("register.html")
+@auth.route("/register", methods=["GET", "POST"])
+def register():
+    if flask.request.method == "POST":
+        username = flask.request.form["username"]
+        email = flask.request.form["email"]
+        password = flask.request.form["password"]
+
+    return flask.render_template("register.html")
