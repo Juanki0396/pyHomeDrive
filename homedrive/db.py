0761db92250e6dbf231153ec1974e83d8e204974
Juanki0396 <juanki0396@gmail.com>
2022-07-11 18:06:07 +0200

    Improved documentation and database connection context manager

--- homedrive/db.py
+++ homedrive/db.py
@@ -5,34 +5,47 @@ from flask import Flask, current_app
 from flask.cli import with_appcontext
 
 
-class Database:
+class DatabaseConnection:
     def __init__(self, database_path: str) -> None:
-        self.database_path = database_path
+        """Instanciate a DatabaseConnection handler. It offers a Context Manager to create database connections
+        safely.
 
-    def __enter__(self):
+        Args:
+            database_path (str): Path of the database file
+        """
+        self.database_path = database_path
+        self.db: sqlite3.Connection = None
+
+    def __enter__(self) -> sqlite3.Cursor:
+        """Start a database conection and return a cursor object."""
         self.db = sqlite3.connect(
             self.database_path, detect_types=sqlite3.PARSE_DECLTYPES
         )
 
         self.db.row_factory = sqlite3.Row
-        return self.db
 
-    def __exit__(self, type, value, traceback):
+        return self.db.cursor()
+
+    def __exit__(self, exc_type, exc_value, exc_traceback) -> bool:
+        """Commit or close database connection if any error arise."""
+        if exc_type is sqlite3.Error:
+            print(exc_type)
+            self.db.close()
+            return False
+
+        self.db.commit()
         self.db.close()
-
-        if type is not None:
-            print(type)
-            print(value)
-            print(traceback)
 
         return True
 
 
-def get_db() -> Database:
-    return Database(current_app.config["DATABASE"])
+def get_db() -> DatabaseConnection:
+    """Return a DatabaseConection object for the current Flask app."""
+    return DatabaseConnection(current_app.config["DATABASE"])
 
 
-def init_db():
+def init_db() -> None:
+    """ "Initialize the database using the sql schema."""
     database = get_db()
     with current_app.open_resource("schema.sql") as file:
         sql_command = file.read().decode("utf8")
@@ -49,6 +62,7 @@ def init_db_command():
 
 
 def init_app(app: Flask) -> None:
+    """Register the init-db command in the current app."""
     app.cli.add_command(init_db_command)
 
 
